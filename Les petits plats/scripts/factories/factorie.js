951e035be8dc0dd7f7873c8babd97cc59096ad7f
squal94 <lounis.khadir@wanadoo.fr>
2022-05-06 18:09:42 +0200

    fin de la factorie ficheRecette

--- Les petits plats/scripts/factories/factorie.js
+++ Les petits plats/scripts/factories/factorie.js
@@ -24,24 +24,51 @@ function filtre() {
 
 function ficheRecette(data) {
   const cardMain = document.getElementById("cardMain");
+
   const fiche = document.createElement("div");
   fiche.classList.add("fiche");
+
   const img = document.createElement("img");
   img.classList.add("fiche__vignette");
   img.setAttribute("src", "/assets/recette/recette-limonada-de-coco.jpg");
+
   const titleContainer = document.createElement("div");
   titleContainer.classList.add("fiche__title");
+
   const title = document.createElement("h2");
   title.classList.add("fiche__title--name");
   title.innerText = `${data.name}`;
+
   const time = document.createElement("div");
   time.classList.add("fiche__title__time");
+
   const timePic = document.createElement("img");
   timePic.classList.add("fiche__title__time--pic");
   timePic.setAttribute("src", "/assets/image/lhorloge.png");
+
   const timeString = document.createElement("p");
   timeString.classList.add("fiche__title__time--string");
   timeString.innerText = `${data.time}min`;
+
+  const recetteContainer = document.createElement("section");
+  recetteContainer.classList.add("fiche__recette");
+
+  const recetteIngredient = document.createElement("div");
+  recetteIngredient.classList.add("fiche__recette--ingredient");
+  const recetteIngredientListe = document.createElement("ul");
+
+  for (let i = 0; i < data.ingredients.length; i++) {
+    const listeLi = document.createElement("li");
+    recetteIngredientListe.appendChild(listeLi);
+    let innerli = data.ingredients[i];
+    console.log(innerli);
+    listeLi.innerText = `${innerli.ingredient}`;
+  }
+
+  const recetteTexte = document.createElement("p");
+  recetteTexte.classList.add("fiche__recette--texte");
+  recetteTexte.innerText = `${data.description}`;
+
   cardMain.appendChild(fiche);
   fiche.appendChild(img);
   fiche.appendChild(titleContainer);
@@ -49,6 +76,10 @@ function ficheRecette(data) {
   titleContainer.appendChild(time);
   time.appendChild(timePic);
   time.appendChild(timeString);
+  fiche.appendChild(recetteContainer);
+  recetteContainer.appendChild(recetteIngredient);
+  recetteIngredient.appendChild(recetteIngredientListe);
+  recetteContainer.appendChild(recetteTexte);
 }
 
 ficheRecette(recettes[0]);
