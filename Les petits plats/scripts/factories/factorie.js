4cb53b52a2844a0bb964691df89c14301f83cebb
squal94 <lounis.khadir@wanadoo.fr>
2022-05-10 12:25:45 +0200

    changement du collapse des filtre

--- Les petits plats/scripts/factories/factorie.js
+++ Les petits plats/scripts/factories/factorie.js
@@ -1,20 +1,21 @@
 //------------- importation des recettes de recipes ----------------
 import { recipes } from "../../data/recipes.js";
-let recettes = [];
-function tableauRecette() {
-  for (let i = 0; i < recipes.length; i++) {
-    recettes.push(recipes[i]);
-  }
-}
-tableauRecette();
+let recettes = [...recipes];
+
 // -----------   filtre   ------------
 function filtre() {
   const ingredient = document.querySelector(".ingredient");
   const appareil = document.querySelector(".appareil");
   const ustensile = document.querySelector(".ustensile");
   const ingredientInput = document.createElement("input");
+  ingredientInput.classList.add("ingredientColor");
+  ingredientInput.setAttribute("placeholder", "Ingredients");
   const appareilInput = document.createElement("input");
+  appareilInput.classList.add("appareilColor");
+  appareilInput.setAttribute("placeholder", "Appareils");
   const ustensileInput = document.createElement("input");
+  ustensileInput.classList.add("ustensileColor");
+  ustensileInput.setAttribute("placeholder", "Ustensiles");
   ingredient.appendChild(ingredientInput);
   appareil.appendChild(appareilInput);
   ustensile.appendChild(ustensileInput);
