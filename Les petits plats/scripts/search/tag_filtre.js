37a4de8118394364f9aedaa457590d25bcb927fe
squal94 <lounis.khadir@wanadoo.fr>
2022-05-25 02:44:31 +0200

    fin de la partie fonctionnelle

--- Les petits plats/scripts/search/tag_filtre.js
+++ Les petits plats/scripts/search/tag_filtre.js
@@ -1,18 +1,11 @@
-import {
-  arrayIngredients,
-  arrayAppareils,
-  arrayUstensils,
-  recettes,
-} from "../factories/filtreFactoryAffichage.js";
+import { recettes } from "../factories/filtreFactoryAffichage.js";
 
 const containerIngredient = document.querySelector(".ingredientContent");
 const containerAppareil = document.querySelector(".appareilContent");
 const containerUstensil = document.querySelector(".ustensileContent");
-const containerSuggestion = document.querySelector(".container__suggestion");
 const appareilInput = document.querySelector(".appareilColor");
 const ingredientInput = document.querySelector(".ingredientColor");
 const ustensilInput = document.querySelector(".ustensileColor");
-const changeTag = document.querySelector(".filtres__container");
 const tagContainer = document.querySelector(".tag");
 const ingredientAllLink = containerIngredient.querySelectorAll(".link");
 const appareilAllLink = containerAppareil.querySelectorAll(".link");
@@ -24,10 +17,6 @@ let arrayTag = [];
 let arrayTemp = [];
 let arrayCompare = [];
 let arrayInputPrincipal = [];
-let arrayIngredientTemp = [];
-let arrayUstensilsTemp = [];
-let arrayAppareilsTemp = [];
-
 let ingredientColor = "ingredientColor";
 let ustensileColor = "ustensileColor";
 let appareilColor = "appareilColor";
@@ -215,117 +204,5 @@ captureTag(containerAppareil);
 captureTag(containerUstensil);
 ifTagClose();
 inputFindLink(ingredientInput, ingredientAllLink);
-
-// function findObjectAffichage(source) {
-//   let checker = (arr, target) => target.every((v) => arr.includes(v));
-//   source.addEventListener("click", () => {
-//     let arrayFicheDisplayNone = [];
-//     const ficheArray = document.querySelectorAll(".fiche");
-//     for (let i = 0; i < recettes.length; i++) {
-//       let valueToCompare = [];
-//       let valueOfUstensils = [...recettes[i].ustensils];
-//       let valueOfIngredients = [...recettes[i].ingredients];
-
-//       for (let i = 0; i < valueOfUstensils.length; i++) {
-//         valueToCompare.push(valueOfUstensils[i].toLowerCase());
-//       }
-
-//       valueToCompare.push(recettes[i].appliance.toLowerCase());
-//       valueToCompare.push(recettes[i].name.toLowerCase());
-
-//       for (let i = 0; i < valueOfIngredients.length; i++) {
-//         valueToCompare.push(valueOfIngredients[i].ingredient.toLowerCase());
-//       }
-
-//       if (checker(valueToCompare, arrayTag) === false) {
-//         arrayFicheDisplayNone.push(recettes[i].name);
-//       }
-//     }
-
-//     for (let i = 0; i < ficheArray.length; i++) {
-//       if (
-//         arrayFicheDisplayNone.indexOf(
-//           ficheArray[i].getAttribute("aria-labelledby")
-//         ) !== -1
-//       ) {
-//         ficheArray[i].style.display = "none";
-//       } else if (
-//         arrayFicheDisplayNone.indexOf(
-//           ficheArray[i].getAttribute("aria-labelledby")
-//         ) === -1
-//       ) {
-//         ficheArray[i].style.display = "flex";
-//       }
-//     }
-//   });
-// }
-
-// function ifTagClose() {
-//   tagContainer.addEventListener("mousedown", (e) => {
-//     let cible = e.target;
-//     let cibleClose = cible.getAttribute("class");
-//     if (cibleClose == "btn-close") {
-//       let ciblelabelledby = cible.getAttribute("aria-labelledby");
-//       let index = arrayTag.indexOf(ciblelabelledby);
-//       arrayTag.splice(index, 1);
-//       findObjectAffichage(tagContainer);
-//     }
-//   });
-// }
-
-// function inputFindObject() {
-//   const searchInputPrincipal = document.getElementById("search__Input");
-//   const ficheArray = document.querySelectorAll(".fiche");
-//   searchInputPrincipal.addEventListener("keyup", () => {
-//     let InputSuggestion = "";
-//     for (let i = 0; i < recettes.length; i++) {
-//       let inputValue = searchInputPrincipal.value;
-//       const resultComparing = recettes[i].name.toLowerCase();
-//       if (resultComparing.includes(inputValue.toLowerCase())) {
-//         if (inputValue !== "" && inputValue.length > 2) {
-//           InputSuggestion += `<p class="suggestion">${resultComparing}</p> `;
-//           document.querySelector(".container__suggestion").innerHTML =
-//             InputSuggestion;
-//         } else if (inputValue === "" || inputValue.length < 2) {
-//           ficheArray[i].style.display = "flex";
-//           document.querySelector(".container__suggestion").innerHTML = "";
-//         }
-//       }
-//     }
-//   });
-// }
-
-// function inputFindLink() {
-//   const ingredientAllLink = containerIngredient.querySelectorAll(".link");
-//   ingredientInput.addEventListener("keyup", () => {
-//     for (let i = 0; i < ingredientAllLink.length; i++) {
-//       let inputValue = ingredientInput.value;
-//       const resultComparing = ingredientAllLink[i].textContent.toLowerCase();
-//       if (resultComparing.indexOf(`${inputValue}`) !== -1) {
-//         ingredientAllLink[i].style.display = "block";
-//       } else {
-//         ingredientAllLink[i].style.display = "none";
-//       }
-//     }
-//   });
-// }
-
-// function inputFindObjectClick() {
-//   containerSuggestion.addEventListener("mouseup", (e) => {
-//     arrayTag.push(e.target.textContent);
-//     findObjectAffichage(containerSuggestion);
-//   });
-// }
-
-// function ifTagClose() {
-//   tagContainer.addEventListener("mousedown", (e) => {
-//     let cible = e.target;
-//     let cibleClose = cible.getAttribute("class");
-//     if (cibleClose == "btn-close") {
-//       let ciblelabelledby = cible.getAttribute("aria-labelledby");
-//       let index = arrayTag.indexOf(ciblelabelledby);
-//       arrayTag.splice(index, 1);
-//       findObjectAffichage(tagContainer);
-//     }
-//   });
-// }
+inputFindLink(appareilInput, appareilAllLink);
+inputFindLink(ustensilInput, ustensilAllLink);
