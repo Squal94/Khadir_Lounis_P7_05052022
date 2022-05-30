a360135927812902ab1cbd3acc1b9c2c418e3893
squal94 <lounis.khadir@wanadoo.fr>
2022-05-30 23:58:02 +0200

    Changement complet du code natif du a un bug

--- Les petits plats/scripts/search/tag_filtre.js
+++ Les petits plats/scripts/search/tag_filtre.js
@@ -5,21 +5,21 @@ import { recettes } from "../factories/filtreFactoryAffichage.js";
 const containerIngredient = document.querySelector(".ingredientContent");
 const containerAppareil = document.querySelector(".appareilContent");
 const containerUstensil = document.querySelector(".ustensileContent");
-const appareilInput = document.querySelector(".appareilInput");
-const ingredientInput = document.querySelector(".ingredientInput");
-const ustensilInput = document.querySelector(".ustensileInput");
-const changeTag = document.querySelector(".filtres__container");
+const appareilInput = document.querySelector(".appareilColor");
+const ingredientInput = document.querySelector(".ingredientColor");
+const ustensilInput = document.querySelector(".ustensileColor");
 const tagContainer = document.querySelector(".tag");
 const ingredientAllLink = containerIngredient.querySelectorAll(".link");
 const appareilAllLink = containerAppareil.querySelectorAll(".link");
 const ustensilAllLink = containerUstensil.querySelectorAll(".link");
 const searchInputPrincipal = document.getElementById("search__Input");
+const allFiche = document.querySelectorAll(".fiche");
 
 // Déclaration de tous les arrays utilisés
 let arrayTag = [];
-let arrayIngredientTemp = [];
-let arrayUstensilsTemp = [];
-let arrayAppareilsTemp = [];
+let arrayTemp = [];
+let arrayCompare = [];
+let arrayInputPrincipal = [];
 
 let ingredientColor = "ingredientColor";
 let ustensileColor = "ustensileColor";
@@ -57,141 +57,152 @@ function filterTag(target, color) {
   );
 }
 
-/**
- * Fonction de trie pour l'affichage des recettes dans main
- * @param {source} action est la source de l'event pour lancer le tri
- * Récupération de la valeur cliqué selon l'event et affichage des recettes triée selon l'action
- */
-
-function findObjectAffichage(source) {
-  source.addEventListener("click", () => {
-    let arrayFicheDisplayNone = [];
-    const ficheArray = document.querySelectorAll(".fiche");
-    for (let i = 0; i < recettes.length; i++) {
-      let valueToCompare = [];
-      let valueOfUstensils = [...recettes[i].ustensils];
-      let valueOfIngredients = [...recettes[i].ingredients];
-
-      for (let i = 0; i < valueOfUstensils.length; i++) {
-        valueToCompare.push(valueOfUstensils[i].toLowerCase());
-      }
-
-      valueToCompare.push(recettes[i].appliance.toLowerCase());
-      valueToCompare.push(recettes[i].name.toLowerCase());
-
-      for (let i = 0; i < valueOfIngredients.length; i++) {
-        valueToCompare.push(valueOfIngredients[i].ingredient.toLowerCase());
-      }
-      if (checker(arrayTag, valueToCompare) === false) {
-        arrayFicheDisplayNone.push(recettes[i].name);
-      }
-    }
-
-    for (let i = 0; i < ficheArray.length; i++) {
-      if (
-        arrayFicheDisplayNone.indexOf(
-          ficheArray[i].getAttribute("aria-labelledby")
-        ) !== -1
-      ) {
-        ficheArray[i].style.display = "none";
-      } else if (
-        arrayFicheDisplayNone.indexOf(
-          ficheArray[i].getAttribute("aria-labelledby")
-        ) === -1
-      ) {
-        if (arrayIngredientTemp.length == 0) {
-          ficheArray[i].style.display = "flex";
+function inputPrincipalFilter() {
+  const fichesArray = document.querySelectorAll(".fiche");
+  searchInputPrincipal.addEventListener("keyup", (e) => {
+    let inputValue = e.target.value;
+    if (inputValue !== "" && inputValue.length > 2) {
+      let InputSuggestion = "";
+      for (let i = 0; i < fichesArray.length; i++) {
+        let ficheLabelledby = fichesArray[i]
+          .getAttribute("aria-labelledby")
+          .toLowerCase();
+        if (ficheLabelledby.indexOf(inputValue.toLowerCase()) !== -1) {
+          InputSuggestion += `<p class="suggestion">${ficheLabelledby}</p> `;
+          document.querySelector(".container__suggestion").innerHTML =
+            InputSuggestion;
+          for (let i = 0; i < recettes.length; i++) {
+            let recetteName = recettes[i].name;
+            let arrayIngredients = [];
+            let arrayUstensils = [];
+            if (recetteName.toLowerCase() === ficheLabelledby) {
+              for (let x = 0; x < recettes[i].ingredients.length; x++) {
+                arrayIngredients.push(
+                  recettes[i].ingredients[x].ingredient.toLowerCase()
+                );
+              }
+              for (let x = 0; x < recettes[i].ustensils.length; x++) {
+                arrayUstensils.push(recettes[i].ustensils[x].toLowerCase());
+              }
+              arrayTemp = [
+                ...arrayTemp,
+                ...arrayIngredients,
+                ...arrayUstensils,
+                recettes[i].appliance.toLowerCase(),
+              ];
+            }
+            arrayTemp = [...new Set(arrayTemp)];
+          }
+          arrayInputPrincipal.push(fichesArray[i]);
+        } else {
+          fichesArray[i].style.display = "none";
         }
       }
+    } else {
+      arrayTemp = [];
+      arrayInputPrincipal = [];
+      document.querySelector(".container__suggestion").innerHTML = "";
+      for (let i = 0; i < fichesArray.length; i++) {
+        fichesArray[i].style.display = "flex";
+      }
+    }
+    supprLinkInFiltre(ingredientAllLink, arrayTemp);
+    supprLinkInFiltre(appareilAllLink, arrayTemp);
+    supprLinkInFiltre(ustensilAllLink, arrayTemp);
+  });
+}
+
+function captureTag(action) {
+  action.addEventListener("click", (e) => {
+    arrayCompare = [];
+    let tempBalise = e.target.getAttribute("class");
+    if (tempBalise === "link") {
+      arrayTag.push(e.target.textContent);
+      arrCompare();
     }
   });
 }
 
-/**
- * Fonction de fermeture des Tags
- * @param {tagContainer} action si le clique capturer dans le tag container contient la class btn-close
- * alors recupération de aria-labelledby liée
- * et suppression de cette valeur dans arrayTag
- */
+function arrCompare() {
+  for (let i = 0; i < recettes.length; i++) {
+    let valueToCompare = [];
+    for (let x = 0; x < recettes[i].ingredients.length; x++) {
+      valueToCompare.push(recettes[i].ingredients[x].ingredient.toLowerCase());
+    }
+    for (let x = 0; x < recettes[i].ustensils.length; x++) {
+      valueToCompare.push(recettes[i].ustensils[x].toLowerCase());
+    }
+
+    valueToCompare.push(recettes[i].appliance.toLowerCase());
+
+    if (checker(arrayTag, valueToCompare) === true) {
+      arrayCompare.push(recettes[i].name);
+    }
+  }
+  affichageFiche(arrayCompare);
+  arrayTag = [...new Set(arrayTag)];
+}
+
+function affichageFiche(array) {
+  if (arrayInputPrincipal.length > 0) {
+    for (let i = 0; i < arrayInputPrincipal.length; i++) {
+      let tempCompare = arrayInputPrincipal[i].getAttribute("aria-labelledby");
+      if (array.indexOf(`${tempCompare}`) !== -1) {
+        arrayInputPrincipal[i].style.display = "flex";
+      } else {
+        arrayInputPrincipal[i].style.display = "none";
+      }
+    }
+  } else {
+    for (let i = 0; i < allFiche.length; i++) {
+      let tempCompare = allFiche[i].getAttribute("aria-labelledby");
+      if (array.indexOf(`${tempCompare}`) !== -1) {
+        allFiche[i].style.display = "flex";
+      } else {
+        allFiche[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function supprLinkInFiltre(array, arrayCompare) {
+  if (arrayCompare.length > 1) {
+    for (let i = 0; i < array.length; i++) {
+      let ciblelabelledby = array[i].textContent;
+      if (arrayCompare.indexOf(`${ciblelabelledby}`) !== -1) {
+        array[i].style.display = "flex";
+      } else {
+        array[i].style.display = "none";
+      }
+    }
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      array[i].style.display = "flex";
+    }
+  }
+}
 
 function ifTagClose() {
-  tagContainer.addEventListener("mouseup", (e) => {
+  tagContainer.addEventListener("mousedown", (e) => {
     let cible = e.target;
     let cibleClose = cible.getAttribute("class");
     if (cibleClose == "btn-close") {
       if (arrayTag.length == 1) {
         arrayTag = [];
-        findObjectAffichage(tagContainer);
       } else {
         let ciblelabelledby = cible.getAttribute("aria-labelledby");
         let stringValue = arrayTag.join(",").replace(`,${ciblelabelledby}`, "");
         arrayTag = stringValue.split(",");
-        findObjectAffichage(tagContainer);
       }
     }
+    arrCompare();
   });
 }
-
-/**
- * Fonction de récupération de la valeur de l'input principal
- * @param {searchInputPrincipal} Input l'input principal
- * si la valeur récupérée est supérieure à 3 lettres alors création d'un array des recettes en display bloc.
- * vérification des recettes correspondant a l'entrée et affichage
- * et création de 3 arrays pour chaque ingrédient appareil et ustensils sélectionner
- */
-
-function inputFindObject() {
-  const ficheArray = document.querySelectorAll(".fiche");
-  searchInputPrincipal.addEventListener("keyup", () => {
-    let InputSuggestion = "";
-    for (let i = 0; i < recettes.length; i++) {
-      let inputValue = searchInputPrincipal.value;
-      const resultComparing = recettes[i].name.toLowerCase();
-      if (resultComparing.indexOf(`${inputValue.toLowerCase()}`) !== -1) {
-        if (inputValue !== "" && inputValue.length > 2) {
-          InputSuggestion += `<p class="suggestion">${resultComparing}</p> `;
-          document.querySelector(".container__suggestion").innerHTML =
-            InputSuggestion;
-          ficheArray[i].style.display = "flex";
-          let arrayTampon = [...recettes[i].ingredients];
-          for (let x = 0; x < arrayTampon.length; x++) {
-            arrayIngredientTemp.push(
-              `${arrayTampon[x].ingredient.toLowerCase()}`
-            );
-          }
-          for (let x = 0; x < recettes[i].ustensils.length; x++) {
-            arrayUstensilsTemp.push(
-              `${recettes[i].ustensils[x].toLowerCase()}`
-            );
-          }
-          arrayAppareilsTemp.push(`${recettes[i].appliance.toLowerCase()}`);
-        }
-        if (inputValue === "" || inputValue.length < 2) {
-          ficheArray[i].style.display = "flex";
-          document.querySelector(".container__suggestion").innerHTML = "";
-        }
-      } else {
-        ficheArray[i].style.display = "none";
-      }
-    }
-    supprLinkInFiltre(ingredientAllLink, arrayIngredientTemp);
-    supprLinkInFiltre(ustensilAllLink, arrayUstensilsTemp);
-    supprLinkInFiltre(appareilAllLink, arrayAppareilsTemp);
-  });
-}
-
-/**
- * Fonction de récupération de la valeur de l'input principal
- * @param {searchInputPrincipal} Input l'input principal
- * si la valeur récupérée est supérieure à 3 lettres alors création d'un array des recettes en display bloc.
- * vérification des recettes correspondant a l'entrée et affichage
- * et création de 3 arrays  ingrédient appareil et ustensils lié au recette encore afficher
- */
 
 function inputFindLink(action, array) {
-  action.addEventListener("keyup", () => {
+  action.addEventListener("keyup", (e) => {
     for (let i = 0; i < array.length; i++) {
-      let inputValue = action.value;
+      let inputValue = e.target.value;
       const resultComparing = array[i].textContent.toLowerCase();
       if (resultComparing.indexOf(`${inputValue}`) !== -1) {
         array[i].style.display = "block";
@@ -201,20 +212,6 @@ function inputFindLink(action, array) {
     }
   });
 }
-
-// function inputFindObjectClick() {
-//   containerSuggestion.addEventListener("mouseup", (e) => {
-//     arrayTag.push(e.target.textContent);
-//     findObjectAffichage(containerSuggestion);
-//   });
-// }
-
-/**
- * Fonction de trie qui nous permet de vérifier les valeurs à afficher si l'input principal est actif et les sous filtre aussi
- * @param {array} arrayTag arrayTag est l'array lié au tag s'ils sont actifs
- * @param {array} valueToCompare valueToCompare est l'array lié à l'input principal s'il est actif
- *si une valeur d'ingrédient ustensile ou appareil dans arraytag est trouvées dans les valeurs des recettes liées à valuetocompare alors on retourne true sinon on retourne false
- */
 
 function checker(fixedArray, inputArray) {
   let fixedArraylen = fixedArray.length;
@@ -231,39 +228,14 @@ function checker(fixedArray, inputArray) {
   return true;
 }
 
-/**
- * Fonction de suppression des valeur contenue dans les filtres
- * @param {array} array est l'array de tous les liens disponible dans chaque filtre
- * @param {arrayCompare}  array  est l'array lié à tous les ingrédients ustensiles et appareils liés aux recettes sélectionner dans l'input principal
- *si la valeur entre les 2 tableaux est trouvée alors le lien reste actif sinon il passe en display none
- */
-
-function supprLinkInFiltre(array, arrayCompare) {
-  // arrayTag = [];
-  if (arrayCompare.length > 0) {
-    for (let i = 0; i < array.length; i++) {
-      let ciblelabelledby = array[i].getAttribute("aria-labelledby");
-      if (arrayCompare.indexOf(`${ciblelabelledby}`) !== -1) {
-        array[i].style.display = "block";
-      } else {
-        array[i].style.display = "none";
-      }
-    }
-  } else {
-    for (let i = 0; i < array.length; i++) {
-      array[i].style.display = "block";
-    }
-  }
-}
-
-// initialisation des funtions
-
-findObjectAffichage(changeTag);
 filterTag(containerIngredient, ingredientColor);
 filterTag(containerAppareil, appareilColor);
 filterTag(containerUstensil, ustensileColor);
+inputPrincipalFilter();
+captureTag(containerIngredient);
+captureTag(containerAppareil);
+captureTag(containerUstensil);
 ifTagClose();
 inputFindLink(ingredientInput, ingredientAllLink);
 inputFindLink(appareilInput, appareilAllLink);
 inputFindLink(ustensilInput, ustensilAllLink);
-inputFindObject();
